11a85481283126204bb5746e51a6282b130edfcb
ayushkumar7842 <ayushkumar800900@gmail.com>
2025-07-02 01:28:01 +0530

    handling-request-response

--- index.js
+++ index.js
@@ -8,8 +8,27 @@ const PORT = 3000;
 const filePath = path.join(__dirname, "example.html");
 
 const server = http.createServer(async (req, res) => {
-  const content = await fs_promises.readFile(filePath, { encoding: "utf-8" });
-  res.end(content);
+  if (req.url == "/home") {
+    const homeContent = await fs_promises.readFile("home.html", {
+      encoding: "utf-8",
+    });
+    res.end(homeContent);
+  } else if (req.url == "/contact") {
+    const contactContent = await fs_promises.readFile("contact.html", {
+      encoding: "utf-8",
+    });
+    res.end(contactContent);
+  } else {
+    res.end("<h1>No Page Found</h1>");
+  }
+  // Handling the HTTP method
+  //   if (req.method == "GET") {
+  //     console.log("request url");
+  //     console.log(req.url);
+  //     res.end("This is GET method");
+  //   } else {
+  //     res.end("This is some other method");
+  //   }
 });
 
 // server is started and listening at PORT 3000
