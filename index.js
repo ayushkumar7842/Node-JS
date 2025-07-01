178de19372c7d1e55ec3b0fda7797b94a3270219
ayushkumar7842 <ayushkumar800900@gmail.com>
2025-07-02 00:30:04 +0530

    sending-html-as-response

--- index.js
+++ index.js
@@ -1,9 +1,15 @@
 const http = require("http");
+const fs_promises = require("fs/promises");
+const path = require("path");
 
 const PORT = 3000;
 
-const server = http.createServer((req, res) => {
-  res.end("<h1>Hello World</h1>");
+// file path of the html home page
+const filePath = path.join(__dirname, "example.html");
+
+const server = http.createServer(async (req, res) => {
+  const content = await fs_promises.readFile(filePath, { encoding: "utf-8" });
+  res.end(content);
 });
 
 // server is started and listening at PORT 3000
